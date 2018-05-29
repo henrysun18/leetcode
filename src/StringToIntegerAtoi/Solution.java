d27bd5123a2f1c1ea339e8494ec6128a60a3bcec
henrysun18 <henrysun12345@hotmail.com>
2018-05-29 13:17:02 -0400

    problem 8 draft

---
+++ src/StringToIntegerAtoi/Solution.java
@@ -0,0 +1,39 @@
+package StringToIntegerAtoi;
+
+/**
+ * Created by Henry on 5/26/2018.
+ * Problem 8
+ */
+public class Solution {
+	public int myAtoi(String str) {
+
+		String strWithoutLeadingSpaces = str.trim();
+		int strWithoutLeadingSpacesLength = strWithoutLeadingSpaces.length();
+		if (strWithoutLeadingSpaces == "") return 0;
+
+		boolean isNegative = false;
+		int charIndex = 0;
+		if (strWithoutLeadingSpaces.charAt(charIndex) == '-') {
+			isNegative = true;
+			charIndex++;
+		} else if (strWithoutLeadingSpaces.charAt(charIndex) == '+') {
+			charIndex++;
+		}
+
+		if (charIndex >= strWithoutLeadingSpacesLength) return 0;
+		char nextChar = strWithoutLeadingSpaces.charAt(charIndex);
+		long convertedNum = 0;
+
+		while (nextChar >= '0' && nextChar <= '9') {
+			convertedNum = convertedNum * 10 + Character.getNumericValue(nextChar);
+
+			if (isNegative && -convertedNum <= Integer.MIN_VALUE) return Integer.MIN_VALUE;
+			if (!isNegative && convertedNum >= Integer.MAX_VALUE) return Integer.MAX_VALUE;
+
+			charIndex++;
+			if (charIndex >= strWithoutLeadingSpacesLength) break;
+			nextChar = strWithoutLeadingSpaces.charAt(charIndex);
+		}
+		return isNegative ? -(int)convertedNum : (int)convertedNum;
+	}
+}

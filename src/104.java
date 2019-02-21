c92ecddc91e92b90fb18e543d69af4f8cfa0906e
henrysun18 <henrysun12345@hotmail.com>
2019-02-20 23:48:00 -0800

    problem 104

---
+++ src/104.java
@@ -0,0 +1,11 @@
+class MaximumDepthOfBinaryTree {
+	public int maxDepth(TreeNode root) {
+		if (root == null) {
+			return 0;
+		} else if (root.left == null && root.right == null) {
+			return 1;
+		} else {
+			return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+		}
+	}
+}

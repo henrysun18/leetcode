8909aca19fdf5b05f74b48c04843e50a47494a84
henrysun18 <henrysun12345@hotmail.com>
2019-03-06 01:08:43 -0800

    problem 91 dp

--- src/91.java
+++ src/91.java
@@ -1,39 +1,51 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class DecodeWays {
 	public int numDecodings(String s) {
 		char[] chs = s.toCharArray();
+
+		Map<Integer, Integer> decodingsFromStart = new HashMap<>();
 
 		if (s == null || s.length() == 0) {
 			return 0;
 		}
 
-		return numDecodings(chs, 0);
+		return numDecodings(chs, 0, decodingsFromStart);
 	}
 
-	private int numDecodings(char[] chs, int start) {
+	private int numDecodings(char[] chs, int start, Map<Integer, Integer> decodingsFromStart) {
 		if (start == chs.length) {
 			return 1;
+		}
+
+		if (decodingsFromStart.containsKey(start)) {
+			return decodingsFromStart.get(start);
 		}
 
 		if (chs[start] == '0') {
 			return 0;
 		}
 
+		int res = 0;
+
 		if (chs[start] == '1') {
 			if (start+1 < chs.length) {
-				return numDecodings(chs, start+1) + numDecodings(chs, start+2);
+				res = numDecodings(chs, start+1, decodingsFromStart) + numDecodings(chs, start+2, decodingsFromStart);
 			} else {
-				return numDecodings(chs, start+1);
+				res = numDecodings(chs, start+1, decodingsFromStart);
 			}
-		}
-
-		if (chs[start] == '2') {
+		} else if (chs[start] == '2') {
 			if (start+1 < chs.length && chs[start+1] <= '6') {
-				return numDecodings(chs, start+1) + numDecodings(chs, start+2);
+				res = numDecodings(chs, start+1, decodingsFromStart) + numDecodings(chs, start+2, decodingsFromStart);
 			} else {
-				return numDecodings(chs, start+1);
+				res = numDecodings(chs, start+1, decodingsFromStart);
 			}
+		} else {
+			res = numDecodings(chs, start+1, decodingsFromStart);
 		}
 
-		return numDecodings(chs, start+1);
+		decodingsFromStart.put(start, res);
+		return res;
 	}
 }

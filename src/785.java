c4356b0ae0ee055f0c6f43d550f6a8fe77f345e5
henrysun18 <henrysun12345@hotmail.com>
2019-04-15 00:03:04 -0700

    problem 785 (dfs)

--- src/785.java
+++ src/785.java
@@ -2,9 +2,46 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 class IsGraphBipartite {
-	public boolean isBipartite(int[][] graph) {
+
+	public boolean isBipartiteDFS(int[][] graph) {
+		int[] colours = new int[graph.length];
+
+		//for each node, dfs starting from that node and try to 2-colour the graph
+		for (int i = 0; i < graph.length; i++) {
+			if (colours[i] != 0) continue; //don't try to dfs starting at an already coloured node
+
+			if (!dfs(graph, colours, i, 1)) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	private boolean dfs(int[][] graph, int[] colours, int node, int colour) {
+		if (colours[node] == -colour) {
+			return false; //node already coloured, and it's opposite of what we wanted to colour it, so return false
+		}
+		if (colours[node] == 0) {
+			//colour this one, then dfs neighbours
+			colours[node] = colour;
+			for (int neighbour : graph[node]) {
+				if (!dfs(graph, colours, neighbour, -colour)) {
+					return false;
+				}
+			}
+		}
+
+		return true;
+	}
+
+
+
+
+
+	public boolean isBipartiteBFS(int[][] graph) {
 		int colour[] = new int[graph.length];
-		
+
 		//for each node, bfs starting from that node and try to 2-colour the graph
 		for (int i = 0; i < graph.length; i++) {
 			if (colour[i] != 0) continue;

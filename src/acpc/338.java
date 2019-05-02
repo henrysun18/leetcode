644bd1387d2505c8e1415e8bbcc359f8ec7311fb
henrysun18 <henrysun12345@hotmail.com>
2019-05-01 22:11:35 -0400

    problem 338 (slightly modify the recurrence relation. use the subproblem where we bitshift right and compensate 1 if needed INSTEAD OF previously where we look at the MSB, then remove it)

--- src/acpc/338.java
+++ src/acpc/338.java
@@ -5,6 +5,23 @@ class CountingBits {
 		int[] bits = new int[num+1]; //0 to num inclusive
 		//for each num, the number of bits = most significant bit + bits in num-(MSB)
 
+		for (int i = 1; i <= num; i++) {
+			// add 1 if we find that we shifted
+			bits[i] = bits[i >> 1] + (i & 1); //111100 = 011110 + 0; 111111 = 011111 + 1;
+
+			//previously the logic was 111100 = 1 + 011100; 111111 = 1 + 011111
+			//both cases work since they both depend on earlier solutions!
+			//this current method is easier to implement since it involves just a bitshift!
+		}
+
+		return bits;
+	}
+
+	/*
+	public int[] countBits(int num) {
+		int[] bits = new int[num+1]; //0 to num inclusive
+		//for each num, the number of bits = most significant bit + bits in num-(MSB)
+
 		int MSB = 1;
 		for (int i = 1; i <= num; i++) {
 			if (i == MSB*2) {
@@ -16,5 +33,5 @@ class CountingBits {
 		}
 
 		return bits;
-	}
+	}*/
 }

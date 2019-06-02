30bab04997a403cfa4039ac18caa992203c1473a
henrysun18 <henrysun12345@hotmail.com>
2019-06-01 23:58:54 -0400

    problem 543: revisited and cleaned

--- src/543.java
+++ src/543.java
@@ -1,26 +1,25 @@
 class DiameterOfBinaryTree {
+	int max = 0; //can just pass around an int[1] if we don't want global vars
+
 	public int diameterOfBinaryTree(TreeNode root) {
-		//want the max depth of left and max depth of right
 		if (root == null) return 0;
 
-		int[] maxDiameter = new int[1];
-		int leftDepth = 1 + depth(root.left, maxDiameter);
-		int rightDepth = 1 + depth(root.right, maxDiameter);
+		maxDepth(root);
 
-
-		return Math.max(maxDiameter[0], leftDepth + rightDepth);
+		return max;
 	}
 
-	private int depth(TreeNode node, int[] maxDiameter) {
-		if (node == null) return -1;
-		if (node.left == null && node.right == null) return 0;
+	private int maxDepth(TreeNode root) {
+		if (root == null) return -1;
 
-		int leftDepth = 1 + depth(node.left, maxDiameter);
-		int rightDepth = 1 + depth(node.right, maxDiameter);
+		int leftDepth = 1 + maxDepth(root.left);
+		int rightDepth = 1 + maxDepth(root.right);
 
-		if (leftDepth + rightDepth > maxDiameter[0]) {
-			maxDiameter[0] = leftDepth + rightDepth;
+		int diameter = leftDepth + rightDepth;
+		if (diameter > max) {
+			max = diameter;
 		}
+
 		return Math.max(leftDepth, rightDepth);
 	}
 }

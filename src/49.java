f52c6b6423e6acc12e5ea8483f40ddb8ee255cd1
henrysun18 <henrysun12345@hotmail.com>
2019-06-02 19:05:55 -0400

    problem 49 revisited: don't sort strings, instead take char counts as the hashmap key / group identifier

--- src/49.java
+++ src/49.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -7,24 +6,29 @@ import java.util.Map;
 
 class GroupAnagrams {
 	public List<List<String>> groupAnagrams(String[] strs) {
-		// hashmap of key -> list of all anagrams
-		// key can be the sorted version of a word
+		//maybe keep a hashmap of sorted str representation to a list of strs from teh array?
+		//we have to sort all the strings; for length s, slgs time, where we could just instead count the letters in s time
+		Map<String, List<String>> groups = new HashMap<>();
 
-		Map<String, List<String>> map = new HashMap<>();
 		for (String str : strs) {
-			String key = getKey(str);
-			map.putIfAbsent(key, new ArrayList<>());
-			map.get(key).add(str);
+			String identifier = getIdentifier(str);
+			groups.putIfAbsent(identifier, new LinkedList<>());
+			groups.get(identifier).add(str);
 		}
-
-		List<List<String>> result = new LinkedList<>(map.values());
-
-		return result;
+		List<List<String>> res = new ArrayList<>(groups.values());
+		return res;
 	}
 
-	private String getKey(String str) {
-		char[] strChars = str.toCharArray();
-		Arrays.sort(strChars);
-		return String.valueOf(strChars);
+	private String getIdentifier(String str) {
+		int[] charCounts = new int[26];
+		for (char c : str.toCharArray()) {
+			charCounts[c-'a']++;
+		}
+		StringBuilder sb = new StringBuilder();
+		for (int count : charCounts) {
+			sb.append("#");
+			sb.append(count);
+		}
+		return sb.toString();
 	}
 }
